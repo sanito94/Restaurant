fe55a9bc6a2b84720eaf3f4ac25a9375a4585e6a
sanito94 <122975967+sanito94@users.noreply.github.com>
2024-06-08 16:39:57 +0200

    Menu

---
+++ Restaurant/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult FirstItem()
+        {
+            return View();
+        }
+    }
+}
